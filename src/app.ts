fbf034da059878b1d5e19f5f5d3ea81c8deb0133
Nguyễn Tiến Cường <cuong.nguyen@apps-cyclone.com>
2022-11-30 16:58:43 +0700

    Update deploy.

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,7 @@
-import cors from 'cors'
-import express, { NextFunction, Request, Response } from 'express'
-import helmet from 'helmet'
-import morgan from 'morgan'
+import express from 'express'
 import { config } from './configs'
-import { handleError } from './helpers/error'
 import { logger } from './helpers/logger'
 import { rootSocket } from './modules-socket/chat/chat.route.soket'
-import { serverAdapter } from './modules/queue/queue.service'
 import { SocketService } from './modules/socket/socket.service'
 
 const app = express()
@@ -16,34 +11,34 @@ const run = async () => {
     // await database.authenticate()
     // await redisService.connect()
 
-    app.set('trust proxy', true)
-    app.use(cors())
-    app.use(helmet())
-    app.use(express.json())
-    app.use(express.urlencoded({ extended: true }))
-    app.use(
-        morgan('short', {
-            skip: (req) => {
-                return (
-                    req.url.startsWith('/api/queues') ||
-                    req.url.startsWith('/admin/queues') ||
-                    req.url.startsWith('/healthcheck')
-                )
-            },
-        })
-    )
+    // app.set('trust proxy', true)
+    // app.use(cors())
+    // app.use(helmet())
+    // app.use(express.json())
+    // app.use(express.urlencoded({ extended: true }))
+    // app.use(
+    //     morgan('short', {
+    //         skip: (req) => {
+    //             return (
+    //                 req.url.startsWith('/api/queues') ||
+    //                 req.url.startsWith('/admin/queues') ||
+    //                 req.url.startsWith('/healthcheck')
+    //             )
+    //         },
+    //     })
+    // )
 
-    app.use('/admin/queues', serverAdapter.getRouter())
+    // app.use('/admin/queues', serverAdapter.getRouter())
     app.get('/healthcheck', async (req, res) => {
         res.send({ status: 'healthy' })
     })
 
-    // app.use('/users', userRouter)
+    // // app.use('/users', userRouter)
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-        handleError(err, res)
-    })
+    // // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    // app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    //     handleError(err, res)
+    // })
 
     const server = app.listen(port, async () => {
         return logger.info(`Server is listening at port ${port}`)
